f3e1443eddeb8335c99e1fc499c1b24a27fb08a2
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-02-12 02:34:50 +0000

    Match aadPlayTonePitchBend

--- src/Game/PSX/AADVOICE.c
+++ src/Game/PSX/AADVOICE.c
@@ -5,6 +5,8 @@ unsigned short aadPitchTable[85];
 
 unsigned long aadStepsPerCent[85];
 
+short aadStepsPerSemitone[85];
+
 AadSynthVoice *aadAllocateVoice(int priority)
 {
     int i;
@@ -203,4 +205,123 @@ void aadPlayTone(AadToneAtr *toneAtr, unsigned long waveStartAddr, AadProgramAtr
     aadMem->voiceKeyOnRequest |= voice->voiceMask;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/PSX/AADVOICE", aadPlayTonePitchBend);
+void aadPlayTonePitchBend(struct AadToneAtr *toneAtr, unsigned long waveStartAddr, struct AadProgramAtr *progAtr, int midiNote, int volume, int masterVolume, int masterPan, int slotVolume, int masterMasterVol, struct AadSynthVoice *voice, int pitchOffset)
+{
+    //struct AadVolume voiceVol; // stack offset -32
+    //int pitch; // $a1
+    //int finePitch; // $a1
+    //int pitchIndex; // $a3
+
+    struct AadVolume voiceVol;         // stack offset -32
+    unsigned int pitch;                         // $a1
+    unsigned int finePitch;                     // $a1
+    unsigned int pitchIndex;                    // $a3
+    unsigned long tmp;                 // $v0 line 10
+    unsigned long pitchOffset2;
+    int temp_lo;
+    //unsigned long tmp;                 // $v0 line 12
+
+    voiceVol.right = ((volume + 1) * (volume + 1)) - 1;
+    voiceVol.left = ((volume + 1) * (volume + 1)) - 1;
+
+    if (!(aadMem->flags & 0x1))
+    {
+        if (masterPan >= 0x41)
+        {
+            voiceVol.left = (unsigned int)((short)((((volume + 1) * (volume + 1)) - 1)) * ((((0x80 - masterPan) * (0x80 - masterPan))) - 1)) >> 12;
+        }
+        else if (masterPan < 0x3F)
+        {
+            voiceVol.right = ((short)((((volume + 1) * (volume + 1)) - 1)) * ((((masterPan + 1) * (masterPan + 1))) + 1)) >> 12;
+        }
+    }
+
+    {
+        unsigned long masterVolumeSquared; // $v1 line 11
+
+        masterVolumeSquared = (toneAtr->volume + 1) * (toneAtr->volume + 1) - 1;
+        voiceVol.left = (voiceVol.left * masterVolumeSquared) >> 14;
+        voiceVol.right = (voiceVol.right * masterVolumeSquared) >> 14;
+    }
+
+    if (!(aadMem->flags & 0x1))
+    {
+        if (toneAtr->panPosition >= 65)
+        {
+            voiceVol.left = (unsigned int)(voiceVol.right * (((0x80 - toneAtr->panPosition) * (0x80 - toneAtr->panPosition)) - 1)) >> 12;
+        }
+        else if (toneAtr->panPosition < 63)
+        {
+            voiceVol.right = (voiceVol.left * (((toneAtr->panPosition + 1) * (toneAtr->panPosition + 1)) + 1)) >> 12;
+        }
+    }
+
+    {
+        unsigned long masterVolumeSquared; // $v1 line 14
+
+        masterVolumeSquared = (masterVolume + 1) * (masterVolume + 1) - 1;
+        voiceVol.left = (voiceVol.left * masterVolumeSquared) >> 14;
+        voiceVol.right = (voiceVol.right * masterVolumeSquared) >> 14;
+    }
+
+    {
+        unsigned long masterVolumeSquared; // $a1 line 14
+
+        masterVolumeSquared = (progAtr->volume + 1) * (progAtr->volume + 1) - 1;
+        voiceVol.left = (voiceVol.left * masterVolumeSquared) >> 14;
+        voiceVol.right = (voiceVol.right * masterVolumeSquared) >> 14;
+    }
+
+    {
+        unsigned long masterVolumeSquared; // $v1 line 14
+
+        masterVolumeSquared = (slotVolume + 1) * (slotVolume + 1) - 1;
+        voiceVol.left = (voiceVol.left * masterVolumeSquared) >> 14;
+        voiceVol.right = (voiceVol.right * masterVolumeSquared) >> 14;
+    }
+
+    {
+        unsigned long masterVolumeSquared; // $v1 line 14
+
+        masterVolumeSquared = (masterMasterVol + 1) * (masterMasterVol + 1) - 1;
+        voiceVol.left = (voiceVol.left * masterVolumeSquared) >> 14;
+        voiceVol.right = (voiceVol.right * masterVolumeSquared) >> 14;
+    }
+
+    SpuSetVoiceVolume(voice->voiceNum, voiceVol.left, voiceVol.right);
+
+    pitchOffset -= 0x2000;
+    temp_lo = 0x2000 / toneAtr->pitchBendMax;
+    pitchIndex = midiNote  + 60 - toneAtr->centerNote;
+    pitchIndex += pitchOffset / temp_lo;
+    pitchOffset2 = aadStepsPerSemitone[pitchIndex] * (pitchOffset % temp_lo) / temp_lo;
+
+    if (toneAtr->centerFine & 0x80)
+    {
+        pitch = 0x100 - toneAtr->centerFine;
+        finePitch = aadPitchTable[pitchIndex];
+        finePitch -= ((aadStepsPerCent[pitchIndex] * (pitch * 100)) >> 23);
+    }
+    else
+    {
+        pitch = toneAtr->centerFine;
+        finePitch = aadPitchTable[pitchIndex];
+        finePitch += ((aadStepsPerCent[pitchIndex] * (pitch * 100)) >> 23);
+    }
+
+    finePitch += pitchOffset2;
+    SpuSetVoicePitch(voice->voiceNum, finePitch & 0xFFFF);
+    SpuSetVoiceStartAddr(voice->voiceNum, waveStartAddr);
+    SpuSetVoiceADSR1ADSR2(voice->voiceNum, toneAtr->adsr1, toneAtr->adsr2);
+
+    if (toneAtr->mode == 4)
+    {
+        aadMem->voiceReverbRequest |= voice->voiceMask;
+    }
+    else
+    {
+        aadMem->voiceReverbRequest = aadMem->voiceReverbRequest & ~voice->voiceMask;
+    }
+
+    aadMem->voiceKeyOnRequest |= voice->voiceMask;
+}
