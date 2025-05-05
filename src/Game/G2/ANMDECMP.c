1d0a280b0622667154c3d23abc4c81d5859c9618
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-05-05 05:05:21 +0000

    Match _G2Anim_DecompressChannel_AdaptiveDelta

--- src/Game/G2/ANMDECMP.c
+++ src/Game/G2/ANMDECMP.c
@@ -1,6 +1,90 @@
 #include "Game/G2/ANMDECMP.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/G2/ANIMG2", _G2Anim_DecompressChannel_AdaptiveDelta);
+void _G2Anim_DecompressChannel_AdaptiveDelta(G2AnimDecompressChannelInfo *dcInfo, G2AnimChanStatus *status)
+{
+    unsigned short *chanData;
+    int index;
+    int keyData;
+    unsigned short step;
+    unsigned short predictedDelta;
+    int targetKey;
+    int storedKey;
+    int keyCount;
+
+    chanData = &dcInfo->chanData[2];
+
+    storedKey = dcInfo->storedKey;
+    targetKey = dcInfo->targetKey;
+
+    keyCount = dcInfo->keylist->keyCount + 3;
+
+    keyData = status->keyData;
+
+    index = status->index;
+
+    while (storedKey < targetKey)
+    {
+        int temp, temp2, temp3; // not from decls.h
+
+        storedKey++;
+
+        temp = chanData[storedKey >> 2];
+
+        temp2 = temp >> ((storedKey & 0x3) << 2);
+
+        step = _stepSizeTable[index];
+
+        index += _indexTable[temp2 & 0xF];
+
+        if (index < 0)
+        {
+            index = 0;
+        }
+
+        if (index >= 64)
+        {
+            index = 63;
+        }
+
+        predictedDelta = step >> 3;
+
+        temp3 = step & 0x1;
+
+        if ((temp2 & 0x4))
+        {
+            predictedDelta += step;
+        }
+
+        step >>= 1;
+
+        if ((temp2 & 0x2))
+        {
+            predictedDelta += step;
+        }
+
+        step >>= 1;
+
+        if ((temp2 & 0x1))
+        {
+            predictedDelta += step + temp3;
+        }
+
+        if ((temp2 & 0x8))
+        {
+            keyData -= predictedDelta;
+        }
+        else
+        {
+            keyData += predictedDelta;
+        }
+    }
+
+    status->index = index;
+
+    status->keyData = keyData;
+
+    dcInfo->chanData = &chanData[keyCount >> 2];
+}
 
 void _G2Anim_DecompressChannel_Linear(G2AnimDecompressChannelInfo *dcInfo, G2AnimChanStatus *status)
 {
