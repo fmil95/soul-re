d5d9eee9364e69fc74cdbd5d56f7f0ea5b78b772
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-06-03 23:40:43 +0000

    Match metaCmdGetSlotVolume

--- src/Game/PSX/AADSQCMD.c
+++ src/Game/PSX/AADSQCMD.c
@@ -141,7 +141,17 @@ void metaCmdGetChannelBendRange()
 {
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/PSX/AADSQCMD", metaCmdGetSlotVolume);
+void metaCmdGetSlotVolume(AadSeqEvent *event, AadSequenceSlot *slot)
+{
+    int variableNum;
+
+    variableNum = (unsigned char)event->dataByte[0];
+
+    if (variableNum < 128)
+    {
+        aadMem->userVariables[variableNum] = slot->selectedSlotPtr->slotVolume;
+    }
+}
 
 void metaCmdGetSlotPan(AadSeqEvent *event, AadSequenceSlot *slot)
 {
