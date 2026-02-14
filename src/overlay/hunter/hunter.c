d68651a240db53a31df6524fca2946123fadf659
Frozen Burnside <fmilesi_soft@hotmail.com>
2026-02-14 17:48:10 +0000

    Fix report generation

--- src/overlay/hunter/hunter.c
+++ src/overlay/hunter/hunter.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+// this conditional is for the objdiff report
+#ifndef SKIP_ASM
+
 INCLUDE_ASM("asm/nonmatchings/overlay/hunter/hunter", FX_MakeHitFlame);
 
 INCLUDE_ASM("asm/nonmatchings/overlay/hunter/hunter", HUNTER_InitFlamethrow);
@@ -15,3 +18,21 @@ INCLUDE_ASM("asm/nonmatchings/overlay/hunter/hunter", HUNTER_ProjectileEntry);
 INCLUDE_ASM("asm/nonmatchings/overlay/hunter/hunter", HUNTER_Projectile);
 
 INCLUDE_RODATA("asm/nonmatchings/overlay/hunter/hunter", D_88000000);
+
+#else
+
+void FX_MakeHitFlame(void) { };
+
+void HUNTER_InitFlamethrow(void) { };
+
+void HUNTER_Flamethrow(void) { };
+
+void HUNTER_Init(void) { };
+
+void HUNTER_CleanUp(void) { };
+
+void HUNTER_ProjectileEntry(void) { };
+
+void HUNTER_Projectile(void) { };
+
+#endif
