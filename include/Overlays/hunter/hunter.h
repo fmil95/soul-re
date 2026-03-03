cf0b57672f0c02bd35b430400ef46318e289dc80
StableCrimson <StableCrimson@outlook.com>
2026-03-02 20:05:06 -0600

    Match HUNTER_Init

--- include/Overlays/hunter/hunter.h
+++ include/Overlays/hunter/hunter.h
@@ -3,6 +3,24 @@
 
 #include "common.h"
 
+// size: 0x2C
+typedef struct _HunterVars {
+    // offset: 0x0000
+    short startIdx;
+    // offset: 0x0002
+    short endIdx;
+    // offset: 0x0004
+    int rem_time;
+    // offset: 0x0008
+    int timer;
+    // offset: 0x000C
+    short angles[16];
+} HunterVars;
 
-
+// size: 4
+typedef struct _HunterAttributes
+{
+    // offset: 0x0000
+    int timeToFire;
+} HunterAttributes;
 #endif
--- src/Overlays/hunter/hunter.c
+++ src/Overlays/hunter/hunter.c
@@ -1,4 +1,6 @@
 #include "Overlays/hunter/hunter.h"
+#include "Game/MEMPACK.h"
+#include "Game/MONSTER/HUMAN.h"
 
 // this conditional is for the objdiff report
 #ifndef SKIP_ASM
@@ -9,7 +11,52 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/hunter/hunter", HUNTER_InitFlamethrow);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/hunter/hunter", HUNTER_Flamethrow);
 
-INCLUDE_ASM("asm/nonmatchings/Overlays/hunter/hunter", HUNTER_Init);
+void HUNTER_Init(Instance *instance)
+{
+
+    MonsterVars *mv; // not from debug symbols
+    MonsterAttributes *ma; // not from debug symbols
+    HunterVars *vars; // not from debug symbols
+    Instance *weaponInst; // not from debug symbols
+
+    mv = (MonsterVars *)instance->extraData;
+    ma = (MonsterAttributes *)instance->data;
+
+    if (mv->age == 0)
+    {
+        weaponInst = HUMAN_CreateWeapon(instance, 13, ma->leftWeaponSegment);
+    }
+    else
+    {
+        weaponInst = HUMAN_CreateWeapon(instance, 14, ma->leftWeaponSegment);
+    }
+
+    if (weaponInst == NULL)
+    {
+        return;
+    }
+
+    HUMAN_Init(instance);
+    mv->extraVars = NULL;
+
+    if (mv->age != 0)
+    {
+        if (mv != NULL)
+        {
+            vars = (HunterVars *)MEMPACK_Malloc(sizeof(HunterVars), 36);
+
+            if (vars != NULL)
+            {
+                mv->extraVars = vars;
+            }
+        }
+        mv->soulJuice = 24576;
+    }
+    else
+    {
+        mv->soulJuice = 20480;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/hunter/hunter", HUNTER_CleanUp);
 
@@ -21,18 +68,63 @@ INCLUDE_RODATA("asm/nonmatchings/Overlays/hunter/hunter", D_88000000);
 
 #else
 
-void FX_MakeHitFlame(void) { };
+void FX_MakeHitFlame(void) {};
 
-void HUNTER_InitFlamethrow(void) { };
+void HUNTER_InitFlamethrow(void) {};
 
-void HUNTER_Flamethrow(void) { };
+void HUNTER_Flamethrow(void) {};
 
-void HUNTER_Init(void) { };
+void HUNTER_Init(Instance *instance)
+{
 
-void HUNTER_CleanUp(void) { };
+    MonsterVars *mv; // not from debug symbols
+    MonsterAttributes *ma; // not from debug symbols
+    HunterVars *vars; // not from debug symbols
+    Instance *weaponInst; // not from debug symbols
 
-void HUNTER_ProjectileEntry(void) { };
+    mv = (MonsterVars *)instance->extraData;
+    ma = (MonsterAttributes *)instance->data;
 
-void HUNTER_Projectile(void) { };
+    if (mv->age == 0)
+    {
+        weaponInst = HUMAN_CreateWeapon(instance, 13, ma->leftWeaponSegment);
+    }
+    else
+    {
+        weaponInst = HUMAN_CreateWeapon(instance, 14, ma->leftWeaponSegment);
+    }
+
+    if (weaponInst == NULL)
+    {
+        return;
+    }
+
+    HUMAN_Init(instance);
+    mv->extraVars = NULL;
+
+    if (mv->age != 0)
+    {
+        if (mv != NULL)
+        {
+            vars = (HunterVars *)MEMPACK_Malloc(sizeof(HunterVars), 36);
+
+            if (vars != NULL)
+            {
+                mv->extraVars = vars;
+            }
+        }
+        mv->soulJuice = 24576;
+    }
+    else
+    {
+        mv->soulJuice = 20480;
+    }
+}
+
+void HUNTER_CleanUp(void) {};
+
+void HUNTER_ProjectileEntry(void) {};
+
+void HUNTER_Projectile(void) {};
 
 #endif
