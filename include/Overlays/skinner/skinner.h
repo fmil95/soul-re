8711ac086cff4e973c6f171f85d89fb4a3bbd4d6
StableCrimson <StableCrimson@outlook.com>
2026-03-06 06:33:36 -0600

    Match FX_StartInstanceBurrow

--- include/Overlays/skinner/skinner.h
+++ include/Overlays/skinner/skinner.h
@@ -3,6 +3,11 @@
 
 #include "common.h"
 
-
+enum SkinnerAnim
+{
+    SKINNER_ANIM_BURROW_IN = 0,
+    SKINNER_ANIM_BURROW_OUT = 1,
+    SKINNER_NUM_ANIMS = 2,
+};
 
 #endif
--- src/Overlays/skinner/skinner.c
+++ src/Overlays/skinner/skinner.c
@@ -1,4 +1,5 @@
 #include "Overlays/skinner/skinner.h"
+#include "Game/COLLIDE.h"
 
 // this conditional is for the objdiff report
 #ifndef SKIP_ASM
@@ -7,7 +8,30 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/skinner/skinner", MON_GetRandomGroundPoin
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/skinner/skinner", MONSENSE_DetectPlayer);
 
-INCLUDE_ASM("asm/nonmatchings/Overlays/skinner/skinner", FX_StartInstanceBurrow);
+void FX_StartInstanceBurrow(Instance *instance, Level *level, TFace *tface)
+{
+
+    SVector normal;
+    SVector translation; // not from debug symbols
+    Vertex *vertex; // not from debug symbols
+    Position *offset; // not from debug symbols
+
+    COLLIDE_GetNormal(tface->normal, (short *)level->terrain->normalList, &normal);
+
+    instance->halvePlane.a = normal.x;
+    instance->halvePlane.b = normal.y;
+    instance->halvePlane.c = normal.z;
+
+    vertex = &level->terrain->vertexList[tface->face.v0].vertex;
+    offset = &level->terrain->BSPTreeArray[instance->bspTree].globalOffset;
+
+    translation.x = vertex->x + offset->x;
+    translation.y = vertex->y + offset->y;
+    translation.z = vertex->z + offset->z;
+
+    instance->halvePlane.d = -(((instance->halvePlane.a * translation.x) + (instance->halvePlane.b * translation.y) + (instance->halvePlane.c * translation.z)) >> 0xC);
+    instance->halvePlane.flags = 1;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/skinner/skinner", FX_StopInstanceBurrow);
 
@@ -39,36 +63,59 @@ INCLUDE_RODATA("asm/nonmatchings/Overlays/skinner/skinner", D_88000000);
 
 #else
 
-void MON_GetRandomGroundPoint(void) { };
+void MON_GetRandomGroundPoint(void) {};
 
-void MONSENSE_DetectPlayer(void) { };
+void MONSENSE_DetectPlayer(void) {};
 
-void FX_StartInstanceBurrow(void) { };
+void FX_StartInstanceBurrow(Instance *instance, Level *level, TFace *tface)
+{
 
-void FX_StopInstanceBurrow(void) { };
+    SVector normal;
+    SVector translation; // not from debug symbols
+    Vertex *vertex; // not from debug symbols
+    Position *offset; // not from debug symbols
 
-void SKINNER_BurrowInEntry(void) { };
+    COLLIDE_GetNormal(tface->normal, (short *)level->terrain->normalList, &normal);
 
-void SKINNER_BurrowIn(void) { };
+    instance->halvePlane.a = normal.x;
+    instance->halvePlane.b = normal.y;
+    instance->halvePlane.c = normal.z;
 
-void SKINNER_GetBurrowDest(void) { };
+    vertex = &level->terrain->vertexList[tface->face.v0].vertex;
+    offset = &level->terrain->BSPTreeArray[instance->bspTree].globalOffset;
 
-void SKINNER_CalcBurrowingMove(void) { };
+    translation.x = vertex->x + offset->x;
+    translation.y = vertex->y + offset->y;
+    translation.z = vertex->z + offset->z;
 
-void SKINNER_PupateEntry(void) { };
+    instance->halvePlane.d = -(((instance->halvePlane.a * translation.x) + (instance->halvePlane.b * translation.y) + (instance->halvePlane.c * translation.z)) >> 0xC);
+    instance->halvePlane.flags = 1;
+}
 
-void SKINNER_Pupate(void) { };
+void FX_StopInstanceBurrow(void) {};
 
-void SKINNER_PursueEntry(void) { };
+void SKINNER_BurrowInEntry(void) {};
 
-void SKINNER_Pursue(void) { };
+void SKINNER_BurrowIn(void) {};
 
-void SKINNER_HideEntry(void) { };
+void SKINNER_GetBurrowDest(void) {};
 
-void SKINNER_Hide(void) { };
+void SKINNER_CalcBurrowingMove(void) {};
 
-void SKINNER_SurpriseAttackEntry(void) { };
+void SKINNER_PupateEntry(void) {};
 
-void SKINNER_SurpriseAttack(void) { };
+void SKINNER_Pupate(void) {};
+
+void SKINNER_PursueEntry(void) {};
+
+void SKINNER_Pursue(void) {};
+
+void SKINNER_HideEntry(void) {};
+
+void SKINNER_Hide(void) {};
+
+void SKINNER_SurpriseAttackEntry(void) {};
+
+void SKINNER_SurpriseAttack(void) {};
 
 #endif
