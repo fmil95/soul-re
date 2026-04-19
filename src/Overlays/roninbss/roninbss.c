92f1f256c699070117613fbd1ec56263e06f5d14
StableCrimson <StableCrimson@outlook.com>
2026-04-19 12:56:23 -0500

    Match RONINBSS_StartBand

--- src/Overlays/roninbss/roninbss.c
+++ src/Overlays/roninbss/roninbss.c
@@ -1,17 +1,38 @@
 #include "Overlays/roninbss/roninbss.h"
+#include "Game/FX.h"
+#include "Game/MONSTER/MONSTER.h"
 
 // this conditional is for the objdiff report
 #ifndef SKIP_ASM
 
-INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_StartBand);
+INCLUDE_RODATA("asm/nonmatchings/Overlays/roninbss/roninbss", D_88000000);
+
+INCLUDE_RODATA("asm/nonmatchings/Overlays/roninbss/roninbss", D_88000020);
+
+void RONINBSS_StartBand(Instance *instance)
+{
+
+    MonsterVars *mv; // not from debug symbols
+    MonsterAttributes *ma; // not from debug symbols
+    RoninbssVars *vars; // not from debug symbols
+    RoninbssAttributes *attrs; // not from debug symbols
+
+    mv = (MonsterVars *)instance->extraData;
+    ma = (MonsterAttributes *)instance->data;
+    vars = (RoninbssVars *)mv->extraVars;
+    attrs = (RoninbssAttributes *)ma->tunData;
+
+    if (!(mv->auxFlags & 0x20))
+    {
+        FX_StartConstrict(instance, (SVector *)&vars->current_constrict_pos, attrs->constrict_start_seg, attrs->constrict_end_seg);
+        mv->auxFlags |= 0x20;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_StopBand);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_InitConstrict);
 
-INCLUDE_RODATA("asm/nonmatchings/Overlays/roninbss/roninbss", D_88000000);
-
-INCLUDE_RODATA("asm/nonmatchings/Overlays/roninbss/roninbss", D_88000020);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_Constrict);
 
@@ -69,7 +90,10 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_Pursue);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_DoNothingEntry);
 
-void RONINBSS_DoNothing() { };
+void RONINBSS_DoNothing(Instance *instance)
+{
+    (void)instance;
+};
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_DeadEntry);
 
@@ -77,72 +101,90 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/roninbss/roninbss", RONINBSS_Dead);
 
 #else
 
-void RONINBSS_StartBand(void) { };         
+void RONINBSS_StartBand(Instance *instance)
+{
 
-void RONINBSS_StopBand(void) { };        
+    MonsterVars *mv; // not from debug symbols
+    MonsterAttributes *ma; // not from debug symbols
+    RoninbssVars *vars; // not from debug symbols
+    RoninbssAttributes *attrs; // not from debug symbols
 
-void RONINBSS_InitConstrict(void) { };      
+    mv = (MonsterVars *)instance->extraData;
+    ma = (MonsterAttributes *)instance->data;
+    vars = (RoninbssVars *)mv->extraVars;
+    attrs = (RoninbssAttributes *)ma->tunData;
 
-void RONINBSS_Constrict(void) { };          
+    if (!(mv->auxFlags & 0x20))
+    {
+        FX_StartConstrict(instance, (SVector *)&vars->current_constrict_pos, attrs->constrict_start_seg, attrs->constrict_end_seg);
+        mv->auxFlags |= 0x20;
+    }
+}
 
-void RONINBSS_StopSoulSuck(void) { };     
+void RONINBSS_StopBand(void) {};
 
-void RONINBSS_FadeMove(void) { };      
+void RONINBSS_InitConstrict(void) {};
 
-void RONINBSS_ChooseAttack(void) { };   
+void RONINBSS_Constrict(void) {};
 
-void RONINBSS_Collide(void) { };    
+void RONINBSS_StopSoulSuck(void) {};
 
-void RONINBSS_Message(void) { };    
+void RONINBSS_FadeMove(void) {};
 
-void RONINBSS_Query(void) { };     
+void RONINBSS_ChooseAttack(void) {};
 
-void RONINBSS_Init(void) { };     
+void RONINBSS_Collide(void) {};
 
-void RONINBSS_CleanUp(void) { };   
+void RONINBSS_Message(void) {};
 
-void RONINBSS_DamageEffect(void) { };   
+void RONINBSS_Query(void) {};
 
-void RONINBSS_FindClosestMarkerInUnit(void) { };
+void RONINBSS_Init(void) {};
 
-void RONINBSS_GetNextMarkerInSeries(void) { };  
+void RONINBSS_CleanUp(void) {};
 
-void RONINBSS_FindValve(void) { };   
+void RONINBSS_DamageEffect(void) {};
 
-void RONINBSS_IdleEntry(void) { };  
+void RONINBSS_FindClosestMarkerInUnit(void) {};
 
-void RONINBSS_Idle(void) { };    
+void RONINBSS_GetNextMarkerInSeries(void) {};
 
-void RONINBSS_WanderEntry(void) { };  
+void RONINBSS_FindValve(void) {};
 
-void RONINBSS_Wander(void) { };      
+void RONINBSS_IdleEntry(void) {};
 
-void RONINBSS_AttackEntry(void) { };   
+void RONINBSS_Idle(void) {};
 
-void RONINBSS_Attack(void) { };       
+void RONINBSS_WanderEntry(void) {};
 
-void RONINBSS_CombatEntry(void) { };  
+void RONINBSS_Wander(void) {};
 
-void RONINBSS_Combat(void) { };    
+void RONINBSS_AttackEntry(void) {};
 
-void RONINBSS_HitEntry(void) { };   
+void RONINBSS_Attack(void) {};
 
-void RONINBSS_Hit(void) { };       
+void RONINBSS_CombatEntry(void) {};
 
-void RONINBSS_FallEntry(void) { };     
+void RONINBSS_Combat(void) {};
 
-void RONINBSS_Fall(void) { };         
+void RONINBSS_HitEntry(void) {};
 
-void RONINBSS_PursueEntry(void) { };     
+void RONINBSS_Hit(void) {};
 
-void RONINBSS_Pursue(void) { };       
+void RONINBSS_FallEntry(void) {};
 
-void RONINBSS_DoNothingEntry(void) { };   
+void RONINBSS_Fall(void) {};
 
-void RONINBSS_DoNothing(void) { };      
+void RONINBSS_PursueEntry(void) {};
 
-void RONINBSS_DeadEntry(void) { };     
-         
-void RONINBSS_Dead(void) { };                   
+void RONINBSS_Pursue(void) {};
+
+void RONINBSS_DoNothingEntry(void) {};
+
+void RONINBSS_DoNothing(Instance *instance) {};
+
+void RONINBSS_DeadEntry(void) {};
+
+void RONINBSS_Dead(void) {};
 
 #endif
